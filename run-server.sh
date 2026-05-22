30a135f79e62fefaf1d962c351a29b1004a9869c
Z User <z@container>
2026-05-22 15:01:13 +0000

    444084f8-3aa8-4c7c-b4e8-fd9c77fcb608

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/z/my-project/.next/standalone
-export DATABASE_URL="file:./db/custom.db"
+export DATABASE_URL="file:/home/z/my-project/.next/standalone/db/custom.db"
 export PORT=3000
 export HOSTNAME=0.0.0.0
 
